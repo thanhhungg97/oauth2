75d78814f73357c069a3b6c00fe156adc9dda440
Harris Nguyen <harnguyen@axon.com>
2023-08-27 11:59:14 +0700

    format code

--- app/infra/encoder/StringEncoder.scala
+++ app/infra/encoder/StringEncoder.scala
@@ -4,7 +4,6 @@ import domain.service.Encoder
 import zio.{UIO, ZIO}
 
 case class StringEncoder() extends Encoder {
-  override def encode(any: String): UIO[String] = {
+  override def encode(any: String): UIO[String] =
     ZIO.succeed(any.hashCode.toString)
-  }
 }
